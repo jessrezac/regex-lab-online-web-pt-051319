182729b24168c98596265b8b4943b59c69fef4b1
Jess Rezac <jess.rezac@gmail.com>
2019-05-14 21:19:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,6 @@
 require 'pry'
 def starts_with_a_vowel?(word)
   if  word.match(/^[aeiouAEIOU]\w+/) ? true : false
-    return false
-  else
-    return true
-  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
